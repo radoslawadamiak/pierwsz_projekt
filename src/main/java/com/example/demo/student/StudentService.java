3f64f528cbe30a00db2f2a518de9f9fc3b7bf0cb
radoslawadamiak <radekadamiakhm@gmail.com>
2021-11-03 11:29:58 +0100

    pierwsza_wrzutka

---
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,40 @@
+package com.example.demo.student;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public List<Student> getStudents() {
+        return studentRepository.findAll();
+    }
+
+    public void addNewStudent(Student student) {
+        Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
+        if (studentOptional.isPresent()) {
+            throw new IllegalStateException("Zajęty e-mail!");
+        }
+        studentRepository.save(student);
+    }
+
+    public void deleteStudent(Long studentId) {
+        boolean exists = studentRepository.existsById(studentId);
+        if(!exists) {
+            throw new IllegalStateException("Student z takim ID: " + studentId + "nie istnieje!");
+        }
+        studentRepository.deleteById(studentId);
+    }
+}
